4355d6244c9573b0becbf64303fee0bf222217db
Marija Zoldin <zoldin@gmail.com>
2017-08-19 18:30:02 +0200

    use feather for data exchange

--- code/evaluate_python_mdl.py
+++ code/evaluate_python_mdl.py
@@ -5,7 +5,7 @@ from scipy import sparse
 from numpy import loadtxt
 try: import cPickle as pickle   # python2
 except: import pickle           # python3
-
+import feather as ft
 if len(sys.argv) != 4:
     sys.stderr.write('Arguments error. Usage:\n')
     sys.stderr.write('\tpython metrics.py MODEL_FILE TEST_MATRIX METRICS_FILE\n')
@@ -19,19 +19,25 @@ with open(model_file, 'rb') as fd:
     model = pickle.load(fd)
 
 
-lines = loadtxt(test_matrix_file, delimiter=" ", unpack=False,skiprows=2)
-matrix=sparse.coo_matrix((lines[:,2],(lines[:,0],lines[:,1])))
-matrix2 = matrix.tocsr()
+#lines = loadtxt(test_matrix_file, delimiter=" ", unpack=False,skiprows=2)
+#matrix=sparse.coo_matrix((lines[:,2],(lines[:,0],lines[:,1])))
+#matrix2 = matrix.tocsr()
+#labels = matrix2[:, 1].toarray()
+#x = matrix2[:, 2:]
 
-labels = matrix2[:, 1].toarray()
-x = matrix2[:, 2:]
+df = ft.read_dataframe(test_matrix_file)
+labels = df.loc[:,'label']
+x = df.loc[:, df.columns != 'label']
+
+
 
 predictions_by_class = model.predict_proba(x)
 predictions = predictions_by_class[:,1]
 
-precision, recall, thresholds = precision_recall_curve(labels, predictions)
+precision, recall, thresholds = precision_recall_curve(labels.ix[:,0], predictions)
 
 auc = metrics.auc(recall, precision)
 #print('AUC={}'.format(metrics.auc(recall, precision)))
 with open(metrics_file, 'w') as fd:
     fd.write('AUC: {:4f}\n'.format(auc))
+
